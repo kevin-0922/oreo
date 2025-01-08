dd750ad3642ffe9195e72125430bdf5e7a3cbd18
李瑋鴻 <117199474+kevin-0922@users.noreply.github.com>
2025-01-08 21:18:11 +0800

    修改404

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import { AuthProvider } from './context/AuthContext';
 import { FavoriteProvider } from './context/FavoriteContext';
 
 
-
+const basename = process.env.PUBLIC_URL || '';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <ThemeProvider theme={theme}>
         <AuthProvider>
           <FavoriteProvider>
